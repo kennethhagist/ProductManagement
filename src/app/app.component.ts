138cffc54e82d6bd758338bfeaad545fe6778a32
kennethhagist <kenhagist@gmail.com>
2017-12-26 16:33:42 -0800

    adding product management app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { ProductService } from './product.service';
+import { Product } from './product';
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  product = [
+    new Product(1, 'DSLR Camera', 1499.99, '../../assets/camera.jpg'),
+    new Product(2, 'iLaptop', 1299.99, '../../assets/laptop.jpg')
+  ];
+
+  constructor(private _productService: ProductService) {
+    this._productService.updateProducts(this.product);
+    this._productService.productsObservable.subscribe( (products) => {
+      this.product = products;
+    });
+  }
+
 }
